8cb3f4678372fcd2f25c3ca533938a0e508b449e
Endalk Gizaw <end.gizaw@gmail.com>
2026-05-03 23:12:13 -0700

    dfs update

--- main.cpp
+++ main.cpp
@@ -140,6 +140,7 @@ bool dfs(int r, int c,
             return true;
         }
     }
+    return false;
 
 }
 
@@ -181,17 +182,17 @@ int main() {
     // STUDENT WORK:
     // Call your DFS, track visited, and fill parent_r and parent_c
     // ------------------------------------------------------
-    // bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
+    bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
 
     // ------------------------------------------------------
     // STUDENT WORK:
     // If found, print the path
     // ------------------------------------------------------
-    // if (found) {
-    //     printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
-    // } else {
-    //     cout << "\nNo path exists.\n";
-    // }
+    if (found) {
+         printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
+    } else {
+         cout << "\nNo path exists.\n";
+    }
 
     return 0;
 }
